4febd59ffa48f78170c3a40084ab89e560822861
Tom <tom@archetypeinnovations.net>
2022-12-09 11:39:40 -0600

    added get generic user info and issues by user functions

--- database/databaseFunctions.py
+++ database/databaseFunctions.py
@@ -2,13 +2,14 @@ import databaseConnection
 
 
 cursor = databaseConnection.db.cursor()
-
+userId = (1,)
 
 def createAdminUser(firstname, lastname, username, email, city_assigned, state_assigned):
     sql = "INSERT INTO admin_user (firstname, lastname, username, email, city_assigned, state_assigned) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (firstname, lastname, username, email, city_assigned, state_assigned)
     cursor.execute(sql, val)
     databaseConnection.db.commit()
+    
 
 
 def createGenericUser(firstname, lastname, email, username):
@@ -16,6 +17,7 @@ def createGenericUser(firstname, lastname, email, username):
     val = (firstname, lastname, email, username)
     cursor.execute(sql, val)
     databaseConnection.db.commit()
+   
 
 
 def createIssue(street, city, state, zip):
@@ -23,5 +25,30 @@ def createIssue(street, city, state, zip):
     val = (street, city, state, zip)
     cursor.execute(sql, val)
     databaseConnection.db.commit()
+  
+
+def getGenericUserInfo(userId):
+    sql = "SELECT * FROM generic_user WHERE id = %s"
+    val = userId
+    cursor.execute(sql, val)
+    myresult = cursor.fetchall()
+    
+
+    for x in myresult:
+        print(x)
+
+def getIssueByUser():
+    sql = "SELECT generic_user.firstname, generic_user.lastname, issue.street\
+           FROM generic_user INNER JOIN issue ON issue.id = generic_user.issue_id"
+    cursor.execute(sql)
+    myresult = cursor.fetchall()
+    
+
+    for x in myresult:
+        print(x)
+
+getGenericUserInfo(userId)
+
+getIssueByUser()
 
 
