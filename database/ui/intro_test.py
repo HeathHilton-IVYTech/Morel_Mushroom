0818a6866bef38ad40971e46c4bf266de3c22b21
HeathHilton-IVYTech <117121784+HeathHilton-IVYTech@users.noreply.github.com>
2023-04-06 19:56:58 -0400

    Cleaning up the abandoned code

--- database/ui/intro_test.py
+++ database/ui/intro_test.py
@@ -60,7 +60,8 @@ dateField = Entry(root)
 dateField.place(x=200, y=450, width=200)
 
 
-Button(root, text='Submit', command=lambda: (submit()), width=20, bg='brown', fg='white').place(x=180, y=525)
+Button(root, text='Submit', command=lambda: (submit()), width=15, bg='brown', fg='white').place(x=80, y=525)
+Button(root, text='Past Locations', command=lambda: (submittedLocations()), width=15, bg='brown', fg='white').place(x=280, y=525)
 
 ##########################################################
 
@@ -68,27 +69,11 @@ Button(root, text='Submit', command=lambda: (submit()), width=20, bg='brown', fg
 
 def openSubmissionWindow():
     root.destroy()
-    #with open('readme.txt') as f:
-    #lines = f.readlines()
-    
-    #rootSubmission = Tk()
-    #rootSubmission.geometry('500x500')
-    #rootSubmission.title("Morel Mushrooms")
-
-    #titleLabel = Label(rootSubmission, text="Morel Mushrooms Logon", width=20, font=("bold", 20))
-    #titleLabel.place(x=90, y=53)
-
-    
-    #Button(rootSubmission, text='Submit', command=lambda: (submit()), width=20, bg='brown', fg='white').place(x=180, y=380)
 
 
 
 #The above Submit button directs the program here. 
 def submit():
-    #Pulling the data in the entry boxes
-    #global rootSubmission
-    #rootSubmission = Tk()
-    #addressField = Entry(rootSubmission)
 
     address = addressField.get()
     city = cityField.get()
@@ -105,68 +90,12 @@ def submit():
         print(content)
         f.close()
         
-        openAdminWindow()
-        #openSubmittedConfirmWindow()
-
-    #Verify that all of the fields have data, program shuts you down unless its filled
-    #if(fullName == "" or street == "" or city == "" or state == "" or emailAddress == "" or issueDescription == ""):
-        #messagebox.showerror('Error', 'All fields are required')
-    #    testFunction = False
-    #If you are good, YOU SHALL PASS!
-    #else:
-        #Attempt to send it to the database, shut it all down if it fails
-        #if(databaseFunctions.createIssue(fullName, street, city, state, emailAddress, issueDescription)):
-        #    print("Database Success.")
-        #    #Attempt to send it to the supplied email
-        #    if(confirmationEmail(fullName, street, city, state, emailAddress, issueDescription)):
-        #        print("Email Success.")
-        #        testFunction = True
-        #    else:
-        #        print("Email Fail.")
-        #        testFunction = False
-        #else:
-        #    print("Database Fail.")
-        #    testFunction = False
-
-    #If there was a failure anywhere, an error is thrown and then it is returned to the function that originally called this function "openNewWindow()"
-    #return(testFunction)
-
-#Take in the field data and then 
-def openSubmittedConfirmWindow():
-    #mail_subject = "Road Tech Repair Submission"
-    #mail_message = "Thank you, " + fullName + ", the issue at " + street + " in " + city + ", " + state + " has been submitted." + issueDescription
-    print("Shut Up, YOU!")
-
-    #my_mailer = mailer.Mailer("py_mailer@clond.net","ZAhJDErU3QK8","smtp.gmail.com")
-    #return my_mailer.send_mail(emailAddress, mail_subject, mail_message)
-
-
-def openNewWindow():
-    #If everything went well, enjoy a box of "toast message" 
-    if(submit()):
-        text="Your Issue has been submitted ... \n\n" + str(submitterName.get()) + "\n" + str(submitterEmail.get()) +"\n" + str(streetAddress.get()) + "\n" + str(submittedIssue.get())
-        messagebox.showinfo("Success", text)
-    #If there was a failure along the way, it sorts out what it was(ish) and lets you know the error
-    else:        
-        fullName = submitterName.get()
-        street = streetAddress.get()
-        city = cityAddress.get()
-        state = stateAddress.get()
-        emailAddress = submitterEmail.get()
-        issueDescription = submittedIssue.get()
-        #Better have submitted something is all of the those fields
-        if(fullName == "" or street == "" or city == "" or state == "" or emailAddress == "" or issueDescription == ""):
-            messagebox.showerror('Error', 'All fields are required')
-        #It wasn't the missing fields that caused it so it's generic message for you.
-        else:
-            messagebox.showerror('Error', 'Something went wrong')
-
     
     
 #The admin window to review the item submitted
-def openAdminWindow():
+def submittedLocations():
     window = tk.Tk()
-    window.title("Issues Submitted")
+    window.title("Submitted Locations")
     
     #ScrollView window was chosen to accomidate how many issues are to be submitted since Indiana roads are salty garbage
     scrollViewBox = tk.Text(window, height=50, width=175)
@@ -182,14 +111,9 @@ def openAdminWindow():
     content = f.read()
     print(content)
     f.close()
-    submittedIssueList = content #databaseFunctions.getAllIssues()
+    submittedIssueList = content
     d = submittedIssueList
-    #Using Panda's Dataframe to manage the data and then spell out the metadata for the column names
-    #df=pd.DataFrame(df, columns=['#', 'Name:', 'Address:', 'City:', 'State:', 'Notes:', 'Date:'])
-    df = pd.read_csv('submissions.csv', sep=",")#index_col=0)
-    #df = pd.DataFrame(data=data).T
-    #Dropping one of the unused fields, added mostly for example purposes
-    #df=df.drop(df.columns[[8]], axis=1)
+    df = pd.read_csv('submissions.csv', sep=",")
 
     scrollViewBox.insert(tk.END, df)
     scrollViewBox(df)
